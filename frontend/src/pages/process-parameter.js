a3a4d26634b4694d0f4e1b90f727c3236dfb68af
UmaJ56 <uma.jaganathan@qbiccatalyst.com>
2026-07-16 20:42:49 +0530

    Changes by client

--- frontend/src/pages/process-parameter.js
+++ frontend/src/pages/process-parameter.js
@@ -1324,7 +1324,7 @@ export default function ProcessParameterPage() {
                 />
               ) : (
                 <div className={styles.messageBox}>
-                  Open the selected department to view its process parameter form.
+                  Select Sub Department to view respective process parameter form.
                 </div>
               )}
               {showFooter ? (
